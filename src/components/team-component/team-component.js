07b4675c4c8d2157c80f5620d224cdf7d01ffb26
eldom93 <eldomiatyn@yahoo.com>
2020-08-16 22:17:11 -0500

    add: add and remove chan functionality

--- src/components/team-component/team-component.js
+++ src/components/team-component/team-component.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './team-component.css';
+import TeamList from '../team-list/team-list';
 
 class TeamComponent extends Component {
     constructor(props) {
         super(props);
         this.state ={
-            disabled: true
+            disabled: true,
+            channelInput: ''
         }
         this.team = this.props.team;
         this.teamIndex = this.props.teamIndex;
@@ -28,21 +30,44 @@ class TeamComponent extends Component {
             }
     }
 
-    removeChannel(index) {
-        console.log(index.target.id);
+    removeChannel(name, index) {
+//console.log(this.team.channels);
+
+ this.setState({
+    teams:   this.team.channels.splice(this.team.channels[index -1], 1)
+})   
     }
 
-    addChannel() {
+    addChannel(event) {
+        this.setState({
+            teams:  this.team.channels.push( {
+                name: this.state.channelInput,
+                index: this.team.channels.length + 1
+              })
+        })
+
         
     }
     
-    sort() {
-        
+    sort(a, b) {
+        var z = Array.from(this.team.channels).sort((a, b) => a - b);
+        console.log(z);
+        this.team.channels.sort(function(a,b){
+            if (a > b) {
+                return -1;
+            }
+            if (b > a) {
+                return 1;
+            }
+            return 0;
+        })
+        console.log(z)
+         
     }
     saveInput(e){
         this.formValidation(e.target.value);
         this.setState({
-            input: e.target.value
+            channelInput: e.target.value
         })
     }
     render() {
@@ -52,11 +77,12 @@ class TeamComponent extends Component {
                 this.team && 
                 <div>
                     <span className="team-name">{this.team.name}</span>
-                    <button className="sort">&#8597;</button>
+                    <button id={this.team.channels} onClick={(e)=> this.sort(e)} className="sort">&#8597;</button>
+              
                     <span className="add-channel">
                         <input onChange={(e)=> this.saveInput(e)} placeholder="Channel name"/>
                         {this.state.disabled ? (<button disabled>&#8853;</button>) : (
-                        <button  onClick={(event)=> this.addTeam(event)}>&#8853;</button>
+                        <button onClick={(event)=> this.addChannel(event)}>&#8853;</button>
                     )}
                     </span>
                 </div>
@@ -64,10 +90,11 @@ class TeamComponent extends Component {
             {
                 this.team &&
                 <ul className="one">
+                
                     { this.team.channels && this.team.channels.map((channel, idx) => (
-                        <li className="channel-name" key={channel.index}>
-                            <span>{channel.name}</span>
-                            <button id={idx} onClick={(index)=> this.removeChannel(index)}>&#8854;</button>
+                     <li className="channel-name" key={channel.index}>
+                            <span channel={channel}>{channel.name}</span>
+                            <button id={idx} onClick={(idx)=> this.removeChannel(channel.name, idx)}>&#8854;</button>
                         </li>
                     ))}
                 </ul>
